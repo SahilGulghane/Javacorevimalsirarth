a0fbecc81490f0f20898718e40fd3dedf81b4411
Sahil Gulghane <101868182+SahilGulghane@users.noreply.github.com>
2023-04-24 23:02:32 +0530

    Update datastream.java

--- datastream.java
+++ datastream.java
@@ -1,1 +1,22 @@
+//Java DataOutputStream class allows an application to write primitive Java data types to the output stream in a machine-independent way.
+//Java application generally uses the data output stream to write data that can later be read by a data input stream.
+
+
+//Java DataInputStream class allows an application to read primitive Java data types to the input stream in a machine-independent way.
+//java application generally uses the data input stream to read data that can later be write by a data output stream.
+import java.io.*;
+
+public class DataStream {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream obj = new FileOutputStream("zer.txt");
+        DataOutputStream obj2 = new DataOutputStream(obj);
+        obj2.writeFloat(33.3f);
+        obj2.writeChars("hello sahil");
+        System.out.println("done");
+        FileInputStream obj3 = new FileInputStream("zer.txt");
+        DataInputStream obj4 = new DataInputStream(obj3);
+        System.out.println(obj4.readFloat());
+        //System.out.println(obj4.readChar()); -> only h
+    }
+}
 
